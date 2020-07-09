7177897c5038d1a95b40342d3358fa0ce00d7791
NQWL <312785889@qq.com>
2020-07-09 16:05:28 +0800

    2020年7月9日16:02:58 项目发布远程服务器，修改了几个配置 1、排除springboot自带服务器 4、远程服务器接口和yml里面的改成一样 2、增加打包方式，增加打包插件，打什么包用什么插件 3、大佬牛逼！

--- src/main/java/com/demo/vchat/vchat/VchatApplication.java
+++ src/main/java/com/demo/vchat/vchat/VchatApplication.java
@@ -3,11 +3,18 @@ package com.demo.vchat.vchat;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.demo.vchat.vchat.mapper")
-public class VchatApplication {
+public class VchatApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VchatApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VchatApplication.class, args);
